155d3f90919a8450f00f366e7ae461f2d2ed8778
Jeremy <leezonghan1997@gmail.com>
2022-09-17 17:14:44 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/zhlee1997/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zhlee1997.my_tiktok_hobby_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_tiktok_hobby_app/app/app.dart';
+import 'package:my_tiktok_hobby_app/bootstrap.dart';
+import 'package:my_tiktok_hobby_app/firebase_options_dev.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await bootstrap(() => const App());
+}
